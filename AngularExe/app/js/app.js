821d07661a0c9832f005821a3f059c24308e8b8d
Ola Popp <olapopp85@gmail.com>
2017-08-15 21:56:36 +0200

    w

--- AngularExe/app/js/app.js
+++ AngularExe/app/js/app.js
@@ -19,11 +19,18 @@ var eventsApp = angular.module("eventsApp", ["ngSanitize", "ngResource", "ngRout
                 templateUrl: "templates/EventDetails.html",
                 controller: "EventController",
                 resolve: {
-                    event:function($route, eventData) {
+                    event: function ($route, eventData) {
                         return eventData.getEvent($route.current.pathParams.eventId).$promise;
                     }
                 }
             });
+        $routeProvider.when("/sampleDirectives",
+            {
+                templateUrl: "templates/SampleDirectives.html",
+                controller: "SampleDirectiveController"
+
+            });
+
         $routeProvider.otherwise({ redirectTo: "/events" });
         $locationProvider.html5Mode(true);
     });
